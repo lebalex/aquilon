42373e2e158c018e466a90e4f634a7f8b9fe030a
lebalex <lebalex@mail.ru>
2020-11-06 08:18:06 +0300

    06.11.2020 13:18

--- index.php
+++ index.php
@@ -17,18 +17,6 @@ $product_id = getParam('product', -1);
         $(window).load(function() {
           var o = $('.sf-menu');
           o.superfish();
-          /*o.find('li a').each(function() {
-            if ($(location).attr('href').indexOf($(this).attr('href')) >= 0) {
-              $(this).addClass('active');
-              return;
-            }
-          })*/
-          /*if (o.parents('aside').length) {
-            var width = $('.container').outerWidth() - $('aside').outerWidth();
-            o.find('.sf-mega').each(function() {
-              $(this).width(width);
-            })
-          }*/
         });
       })(jQuery);
     </script>

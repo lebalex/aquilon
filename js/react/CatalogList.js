1ff7c408975a4eb1f4201d852a6624bf98b13163
lebalex <lebalex@mail.ru>
2020-11-08 06:28:13 +0300

    08.11.20202 11:28

--- js/react/CatalogList.js
+++ js/react/CatalogList.js
@@ -232,7 +232,7 @@ export class CatalogList extends React.Component {
     item.children.forEach(element => {
       if (element.children != null) {
         LIST.push(<li className="sf-with-mega">
-          <a href="#"><span>{element.name}</span></a>
+          <a href="#" className="sf-with-ul2"><span>{element.name}</span></a>
           <ul className="sf-mega" style={{ backgroundImage: "url(/img/menu-bg-887x420.jpg)" }}>
             <li className="sf-mega_row">
               <div className="sf-mega_section">
